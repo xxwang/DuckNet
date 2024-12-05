a77316c1ad52540087769cc246fd614f49f69419
xxwang <78023776@qq.com>
2024-12-05 09:44:51 +0800

    no message

--- Package.swift
+++ Package.swift
@@ -6,13 +6,14 @@ import PackageDescription
 let package = Package(
     name: "DuckNet",
     platforms: [
-        .iOS(.v15)
+        .iOS(.v15),
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "DuckNet",
-            targets: ["DuckNet"]),
+            targets: ["DuckNet"]
+        ),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
